57551397925d024bdb29ea8d4b68befcc1881bc2
IlyaN <naumov.dds@yandex.ru>
2024-04-23 22:23:15 +0300

    Updates

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+// App.tsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Sidebar from './components/Sidebar/Sidebar';
+import { Container, Row, Col } from 'react-bootstrap';
+import routes from './shared/routes';
+import FinanceProvider from './shared/FinanceContext';
+import store from './shared/store';
+import './App.css'
+
+const App: React.FC = () => {
+  console.log(store.getState());
+  return (
+    <FinanceProvider>
+      <Router>
+          <Container fluid>
+            <Row>
+              <Col className='d-flex' style={{padding:'15px', backgroundColor: '#ff6f32'}} >
+                <img className='me-2 mb-2' src='/investment-analysis-icon.svg' width={'50px'}/>
+                <h2>Finance control</h2>
+              </Col>
+            </Row>
+            <Row>
+              <Col className='col-auto px-0 sidebar-block'>
+                <Sidebar />
+              </Col>
+              <Col style={{padding:'15px'}}>
+                <Switch>
+                  {routes.map(route => (
+                    <Route 
+                      key={route.path} 
+                      path={route.path} 
+                      exact={route.exact} 
+                      component={route.component}
+                    />
+                  ))}
+                </Switch>
+              </Col>
+            </Row>
+          </Container>
+        </Router>
+    </FinanceProvider>
+  );
+};
+
+export default App;
---
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import './VideoPlayer.css';
+import { Button } from "react-bootstrap";
+
+const VideoPlayer: React.FC = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const togglePlay = () => {
+    setIsPlaying(!isPlaying);
+  };
+
+  const closeVideo = () => {
+    setIsPlaying(false);
+  };
+
+  const stopPropagation = (e: React.MouseEvent<HTMLVideoElement, MouseEvent>) => {
+    e.stopPropagation();
+  };
+
+  return (
+    <div className="video-container">
+      <Button className="btn btn-primary play-button" onClick={togglePlay}> {"Смотреть"} </Button>
+      {isPlaying && (
+        <div className="video-layout" onClick={closeVideo}>
+            <video className="video-player" controls autoPlay onClick={stopPropagation}> 
+                <source src="/finance.mp4" type="video/mp4" />Your browser does not support the video tag.
+            </video>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default VideoPlayer;
